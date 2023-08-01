8ddf70832ad79a2cac95ddc2f5bfef1508145667
Pratham Tandale <prathamt12345@gmail.com>
2023-08-01 19:23:52 +0530

    sessions in django

--- Learning_Django/tasks/views.py
+++ Learning_Django/tasks/views.py
@@ -3,8 +3,6 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-tasks = ['Eat','Sleep','Code']
-
 class NewTaskFrom(forms.Form):
     task = forms.CharField(label="New Task")
     priority = forms.IntegerField(label="Priority", min_value= 1, max_value = 10)
@@ -12,8 +10,10 @@ class NewTaskFrom(forms.Form):
 # Create your views here.
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, 'tasks/index.html',{
-        'tasks': tasks
+        'tasks': request.session["tasks"]
     })
 
 def add(request):
@@ -21,7 +21,7 @@ def add(request):
         form = NewTaskFrom(request.POST)
         if form.is_valid():
             task = form.cleaned_data['task']
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
