54fe274891e6f30e15edb6bf5cc8f814bf84aff3
Pratham Tandale <prathamt12345@gmail.com>
2023-08-05 22:07:10 +0530

    Handel search bar query

--- Project_Wiki/encyclopedia/views.py
+++ Project_Wiki/encyclopedia/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+from django import forms
 from . import util
 
 markdowner = Markdown()
 
+class search(forms.Form):
+    q = forms.CharField()
+
 def index(request):
+    if request.method == "POST":
+        entry = search(request.POST)
+        if entry.is_valid():
+            q = entry.cleaned_data['q']
+            try:
+                return entryTitle(request, q)
+            except Exception as e:
+                return render(request, "encyclopedia/error.html")
+        else:
+            return render(request, "encyclopedia/error.html")
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
