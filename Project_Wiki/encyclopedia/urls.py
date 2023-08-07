ede8b615e1c59d7ac774eb50aafa1a87610510af
Pratham Tandale <prathamt12345@gmail.com>
2023-08-07 18:16:55 +0530

    Searching efficiently, Working on create new page

--- Project_Wiki/encyclopedia/urls.py
+++ Project_Wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("createnew/", views.createNew, name="createNew"),
     path("<str:title>/", views.entryTitle, name="title")
 ]
--- Project_Wiki/encyclopedia/views.py
+++ Project_Wiki/encyclopedia/views.py
@@ -8,17 +8,25 @@ markdowner = Markdown()
 class search(forms.Form):
     q = forms.CharField()
 
+class new():
+    pass
+
 def index(request):
     if request.method == "POST":
         entry = search(request.POST)
         if entry.is_valid():
             q = entry.cleaned_data['q']
-            try:
+            if q in util.list_entries():
                 return entryTitle(request, q)
-            except Exception as e:
-                return render(request, "encyclopedia/error.html")
+            else:
+                return render(request, "encyclopedia/search.html",{
+                    "entries": filter(lambda x: q.lower() in x.lower(), util.list_entries()),
+                    "q": q
+                })
         else:
-            return render(request, "encyclopedia/error.html")
+            return render(request, "encyclopedia/error.html", {
+                "key" : 404
+            })
 
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -27,9 +35,22 @@ def index(request):
 def entryTitle(request, title):
     entry = util.get_entry(title)
     if not entry:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+                "key" : 404
+            })
     return render(request, "encyclopedia/entry.html", {
         "entry": markdowner.convert(entry),
         "title": title
     })
 
+def createNew(request):
+    if request.method == "POST":
+        title = new(request.POST)
+        if title.is_valid():
+            q = entry.cleaned_data['q']
+            if q in list(filter(lambda x: q.lower() in x.lower(), util.list_entries())):
+                return render(request, "encyclopedia/error.html")
+            else:
+                pass
+
+    return render(request, "encyclopedia/createpage.html")
